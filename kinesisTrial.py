faaa5ee1f3b90974a9d2690e954174be3c9d0e70
Enes Grahovac <enes.grahovac@rokk3rlabs.com>
2018-08-18 12:30:34 -0400

    videoStream

---
+++ kinesisTrial.py
@@ -0,0 +1,15 @@
+import boto3
+STREAM_NAME = "PItest"
+kvs = boto3.client("kinesisvideo")
+# Grab the endpoint from GetDataEndpoint
+endpoint = kvs.get_data_endpoint(
+    APIName="GET_HLS_STREAMING_SESSION_URL",
+    StreamName=STREAM_NAME
+)['DataEndpoint']
+# Grab the HLS Stream URL from the endpoint
+kvam = boto3.client("kinesis-video-archived-media", endpoint_url=endpoint)
+url = kvam.get_hls_streaming_session_url(
+    StreamName=STREAM_NAME,
+    PlaybackMode="LIVE"
+)['HLSStreamingSessionURL']
+print(url)
